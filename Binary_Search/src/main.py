b1b08e817d69684e080cc52dd786d55028007863
Harry Hallows <harry.hallows@gousto.co.uk>
2023-04-27 17:57:10 +0100

    TDD/insert: added basic insertion and node root generation and associated tests

--- Binary_Search/src/main.py
+++ Binary_Search/src/main.py
@@ -1,13 +1,40 @@
+from dataclasses import dataclass
+from typing import Union
+
+
+@dataclass
+class Node:
+    root: Union[int, None]
+    key: int
+    left_branch: Union[list, None]
+    right_branch: Union[list, None]
+
 
 class Tree:
-    def __init__(self):
-        pass
+    """
+    A Binary Search Tree (BST) is a tree data structure where each node has at most two children, and the left subtree
+    of a node contains only nodes with keys less than the node's key, and the right subtree of a node contains only
+    nodes with keys greater than the node's key.
+    """
 
-    def insert(self, key):
+    def __init__(self):
+        self.branches: [Node] = []
+
+    @staticmethod
+    def generate_root_node(node: Node) -> int:
+        node.root = node.key
+        return node.root
+
+    def insert(self, node: Node) -> None:
         """
         Inserts a new node with the given key into the Tree
         """
-        pass
+        # IF no existing node then generate root
+        if len(self.branches) == 0:
+            node.root = self.generate_root_node(node)
+        # IF node key values are less than the root node then add to left branch
+        # ELSE IF node key values are greater than root node then add to right branch
+        self.branches.append(node)
 
     def search(self, key):
         """
--- Binary_Search/tests/test_main.py
+++ Binary_Search/tests/test_main.py
@@ -1,3 +1,38 @@
-from Binary_Search.src.main import Tree
+from Binary_Search.src.main import Tree, Node
+
+
+def test_insert():
+    # Arrange
+    node = Node(
+        root=None,
+        key=1,
+        left_branch=None,
+        right_branch=None
+    )
+
+    tree = Tree()
+
+    # Act
+    tree.insert(node)
+
+    # Assert
+    assert tree.branches[0].key == node.key
+
+
+def test_insert_generates_root_node():
+    # Arrange
+    node = Node(
+        root=None,
+        key=1,
+        left_branch=None,
+        right_branch=None
+    )
+    tree = Tree()
+
+    # Act
+    tree.insert(node)
+
+    # Assert
+    assert tree.branches[0].root == node.key
 
 
