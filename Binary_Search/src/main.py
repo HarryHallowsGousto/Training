ca8a4cd00c05e8678114b4fbe93b0861d68dc1a6
Harry Hallows <harry.hallows@gousto.co.uk>
2023-05-16 17:07:21 +0100

    BST: created the insert and search functions using recursion to achieve both with associated tests

--- Binary_Search/src/main.py
+++ Binary_Search/src/main.py
@@ -1,48 +1,78 @@
 from dataclasses import dataclass
-from typing import Union
+from typing import Optional
 
 
 @dataclass
 class Node:
-    root: Union[int, None]
     key: int
-    left_branch: Union[list, None]
-    right_branch: Union[list, None]
+    left: Optional["Node"] = None
+    right: Optional["Node"] = None
 
 
 class Tree:
     """
     A Binary Search Tree (BST) is a tree data structure where each node has at most two children, and the left subtree
-    of a node contains only nodes with keys less than the node's key, and the right subtree of a node contains only
-    nodes with keys greater than the node's key.
+    of a node contains only nodes with keys less than the current_node's key/root, and the right subtree of a node
+    contains only nodes with keys greater than the node's key/root.
     """
 
     def __init__(self):
-        self.branches: [Node] = []
-
-    @staticmethod
-    def generate_root_node(node: Node) -> int:
-        node.root = node.key
-        return node.root
+        self.root: Optional[Node] = None
 
     def insert(self, node: Node) -> None:
         """
         Inserts a new node with the given key into the Tree
         """
         # IF no existing node then generate root
-        if len(self.branches) == 0:
-            node.root = self.generate_root_node(node)
-        # IF node key values are less than the root node then add to left branch
-        # ELSE IF node key values are greater than root node then add to right branch
-        self.branches.append(node)
+        if self.root is None:
+            self.root = Node(node.key)
+        else:
+            self._insert_recursive(self.root, node)
 
-    def search(self, key):
+    def _insert_recursive(self, root: Node, node: Node):
+        """
+        Runs through a search of all the existing nodes
+        Checks if they have a current value attached to their left or right branches and assigns
+        the passed through node to the correct position
+
+        - IF `node.key` values are less than the `root.key` then add to left branch
+        - ELSE IF `node.key` values are greater than `root.key` then add to right branch
+        """
+        if node.key < root.key:
+            if root.left is None:
+                root.left = node
+            else:
+                self._insert_recursive(root.left, node)
+        else:
+            if root.right is None:
+                root.right = node
+            else:
+                self._insert_recursive(root.right, node)
+
+    def search(self, node: Node) -> Node:
         """
         - Searches for a node with the given key in the Tree, and returns its value
         - Raises exception if the node is not found
-        :returns: Key
+        :returns: node.key
         """
-        pass
+        return self._search_recursive(self.root, node)
+
+    def _search_recursive(self, root: Node, node: Node) -> Node:
+        """
+        Searches for different values from the nodes in the tree and then returns it back once
+        Identifying the specific key input
+        """
+        if root is None:
+            raise ValueError(
+                f"expected node value: {node.key} doesn't exist within tree and therefore can't be searched."
+            )
+        else:
+            if root.key == node.key:
+                return node
+            elif node.key < root.key:
+                return self._search_recursive(root.left, node)
+            elif node.key > root.key:
+                return self._search_recursive(root.right, node)
 
     def delete(self, key):
         """
--- Binary_Search/tests/test_main.py
+++ Binary_Search/tests/test_main.py
@@ -1,38 +1,61 @@
+import pytest
+
 from Binary_Search.src.main import Tree, Node
 
 
 def test_insert():
     # Arrange
-    node = Node(
-        root=None,
-        key=1,
-        left_branch=None,
-        right_branch=None
-    )
-
+    root_key = 10
+    node_key = 12
     tree = Tree()
 
     # Act
-    tree.insert(node)
+    tree.insert(Node(root_key))
+    tree.insert(Node(node_key))
 
     # Assert
-    assert tree.branches[0].key == node.key
+    assert tree.root.right is not None          # Check that the roots right branch is populated
+    assert tree.root.right.key == node_key      # Check the key in the right branch is consistent with the key provided
 
 
 def test_insert_generates_root_node():
     # Arrange
-    node = Node(
-        root=None,
-        key=1,
-        left_branch=None,
-        right_branch=None
-    )
+    node = Node(1)
     tree = Tree()
 
     # Act
     tree.insert(node)
 
     # Assert
-    assert tree.branches[0].root == node.key
+    assert tree.root.key == node.key
 
+
+def test_search_finds_node():
+    # Arrange
+    tree = Tree()
+    tree.insert(Node(0))
+    tree.insert(Node(20))
+    tree.insert(Node(10))
+
+    # Act
+    searched_node = tree.search(Node(10))
+
+    # Assert
+    assert searched_node is not None
+    assert searched_node.key == 10
+
+
+def test_search_does_not_find_node():
+    # Arrange
+    tree = Tree()
+    tree.insert(Node(0))
+    tree.insert(Node(20))
+    tree.insert(Node(10))
+
+    # Act and Assert
+    with pytest.raises(ValueError):
+        # Act
+        searched_node = tree.search(Node(5))
+        # Assert
+        assert searched_node is None
 
